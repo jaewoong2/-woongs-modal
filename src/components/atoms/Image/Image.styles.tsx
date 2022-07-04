368c0736f003e69b9a03f34a486c9cbc03714248
jaewoong2 <jwisgenius@naver.com>
2022-07-04 11:57:36 +0900

    :sparkles: implement skeleton Image

--- src/components/atoms/Image/Image.styles.tsx
+++ src/components/atoms/Image/Image.styles.tsx
@@ -1,7 +1,32 @@
+import { css, keyframes } from '@emotion/react'
 import styled from '@emotion/styled'
 
-export const SkeletonImage = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+const skeletonLoading = keyframes`
+  0% {
+    background-color: hsl(200, 20%, 80%);
+  }
+  100% {
+    background-color: hsl(200, 20%, 95%);
+  }
+`
+
+type SkeletonImageType = {
+  isLoading: boolean
+  skeleton: boolean
+}
+
+export const SkeletonImage = styled.div<SkeletonImageType>`
+  ${({ isLoading, skeleton }) =>
+    isLoading &&
+    skeleton &&
+    css`
+      background-color: #c2cfd6;
+      animation: ${skeletonLoading} 1s linear infinite alternate;
+      border: 8px;
+
+      img {
+        opacity: 0;
+        visibility: hidden;
+      }
+    `}
 `
--- src/components/atoms/Image/index.tsx
+++ src/components/atoms/Image/index.tsx
@@ -1,16 +1,50 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { SkeletonImage } from './Image.styles'
 
 type ImageProps = Omit<React.ImgHTMLAttributes<HTMLImageElement>, 'onLoad' | 'onError'>
 
+/*
+ * auto: 속성을 포함하지 않는 것과 동일한 브라우저의 기본 지연 로딩 동작입니다.
+ * lazy: 뷰포트로부터 계산된 거리에 도달할 때까지 리소스 로딩을 지연시킵니다.
+ * eager: 페이지에서의 위치에 관계없이 리소스를 즉시 로드합니다.
+ */
 const Image = React.forwardRef<HTMLImageElement, ImageProps>(({ ...props }, ref) => {
-  const [, setLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true)
+  const [skeleton, setSkeleton] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (isLoading) {
+        setSkeleton(true)
+      }
+    }, 200)
+
+    return () => clearTimeout(timer)
+  }, [isLoading])
 
   const getError = () => {
-    setLoading(false)
+    setIsLoading(false)
   }
 
-  return <img ref={ref} src={props.src} alt="" onLoad={() => setLoading(false)} onError={getError} {...props} />
+  const onLoad = () => {
+    setIsLoading(false)
+  }
+
+  return (
+    <SkeletonImage skeleton={skeleton} isLoading={isLoading}>
+      <img
+        ref={ref}
+        src={props.src}
+        alt=""
+        loading="lazy"
+        onLoad={onLoad}
+        onError={getError}
+        width={'450px'}
+        height={'250px'}
+        {...props}
+      />
+    </SkeletonImage>
+  )
 })
 
 Image.displayName = 'Image'
